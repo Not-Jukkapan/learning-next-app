ca72514b64d7f17be25679e6ef80cc3d5d81ec56
Jukkapan Kongjun <jukkapan.kn@gmail.com>
2024-10-07 12:58:55 +0700

    Learn how to fetch data -> if possible alway fetch on SSR

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 
-const UsersPage = () => {
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
   return (
-    <div>
-        UserPages
-    </div>
-  )
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
