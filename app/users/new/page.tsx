fafc6044a8dac0857f189088035a50bbd2050b33
Jukkapan Kongjun <jukkapan.kn@gmail.com>
2024-10-08 13:35:02 +0700

    try programmatic navigation in /users  <Link> for SSR useRouter from next/navigation for CSR.

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,8 +1,15 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const NewUserPage = () => {
+  const router = useRouter();
+
   return (
-    <div>NewUserPage</div>
+    <button className='btn btn-primary' onClick={() => router.push('/users')}>
+      Craete
+    </button>
+
   )
 }
 
