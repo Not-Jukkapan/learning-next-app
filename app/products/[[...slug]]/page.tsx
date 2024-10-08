e0d6fbcc4e127ff00d0568882881b909b8fa1812
Jukkapan Kongjun <jukkapan.kn@gmail.com>
2024-10-08 11:29:32 +0700

    Try Catch All segment know [...slug], [[...slug]] ry with products page

---
+++ app/products/[[...slug]]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface Props{
+    params:{
+        slug: string[];
+    }
+}
+
+const ProductPage = ({params:{slug}}:Props) => {
+  return (
+    <div>
+      Product Page {slug}
+    </div>
+  )
+}
+
+export default ProductPage
