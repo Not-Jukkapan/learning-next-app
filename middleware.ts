4617cc0888deb83e13feb7d269c01e9c4fe44635
Jukkapan Kongjun <jukkapan.kn@gmail.com>
2024-10-09 00:12:34 +0700

    User middleware from next-auth/middleware for protect routes.

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
+// import middleware from "next-auth/middleware";
+export { default } from 'next-auth/middleware';
 
-export function middleware(request:NextRequest){
-    return NextResponse.redirect(new URL('/new-page', request.url)); 
+export const config = {
+    // *: zero or more
+    // +: one or more
+    // ?: zero or one
+    matcher: ['/users/:id']
 }
