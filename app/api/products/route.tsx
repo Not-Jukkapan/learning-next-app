a5c2cb73b85b0e9236d5d17c412220bd1581aa28
Jukkapan Kongjun <jukkapan.kn@gmail.com>
2024-10-08 21:48:13 +0700

    make GET, POST for product route

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,21 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-    return NextResponse.json([
-        {
-            id: 1, name: 'Milk', price: 10
-        },
-        {
-            id: 2, name: 'Egg', price: 5
-        }])
+export async function GET(request: NextRequest) {
+    const products = await prisma.product.findMany();
+    return NextResponse.json(products)
 }
 
 export async function POST(request: NextRequest) {
-
     const body = await request.json();
     const validateion = schema.safeParse(body);
     if (!validateion.success) return NextResponse.json(validateion.error.errors, { status: 400 });
 
-    return NextResponse.json({ id: 10, name: body.name, price: body.price });
+    const newProduct = await prisma.product.create({
+        data: {
+            name: body.name,
+            price: body.price
+        }
+    })
+
+    return NextResponse.json(newProduct, { status: 201 });
 }
